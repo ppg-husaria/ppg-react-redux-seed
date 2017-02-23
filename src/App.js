b58ffc87ced269cf04d97fc65f0661b13c76fd63
spartanPAGE <spartanpageagain@gmail.com>
2017-02-23 22:02:05 +0100

    added enzyme, divided App into smaller components and added tests

--- src/App.js
+++ src/App.js
@@ -2,17 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class AppHeader extends Component {
+  render() {
+    return <header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Welcome to React</h2>
+    </header>
+  }
+}
+
+class AppContent extends Component {
+  render() {
+    return <article>
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </article>
+  }
+}
+
+class AppFooter extends Component {
+  render() {
+    return <footer>
+      Copyright © 2017 Patryk Wertka &and; Mateusz Janota
+    </footer>
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AppHeader />
+        <AppContent />
+        <AppFooter />
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
 });
+
+it('has proper header', () => {
+  const app = mount(<App />);
+  expect(app.find(<header />)).toBeTruthy();
+  expect(app.contains(<h2>Welcome to React</h2>)).toBeTruthy();
+})
+
+it('has proper content', () => {
+  const app = mount(<App />);
+  expect(app.find(<article />)).toBeTruthy();
+})
+
+it('has proper footer', () => {
+  const app = mount(<App />);
+  const footer = <footer>Copyright © 2017 Patryk Wertka &and; Mateusz Janota</footer>;
+
+  expect(app.find(<footer />)).toBeTruthy();
+  expect(app.contains(footer)).toBeTruthy();
+})
