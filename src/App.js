9830ac3f340e49e72710b2a30e0bcf6be25f36e0
spartanPAGE <spartanpageagain@gmail.com>
2017-02-23 23:29:09 +0100

    added gh & fb buttons, made authors shwifty

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import { Jumbotron } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
+
+function intersperse(arr, sep) {
+    if (arr.length === 0) {
+        return [];
+    }
+
+    return arr.slice(1).reduce(function(xs, x, i) {
+        return xs.concat([sep, x]);
+    }, [arr[0]]);
+}
 
 class AppHeader extends Component {
   render() {
@@ -13,16 +24,30 @@ class AppHeader extends Component {
 
 class AppContent extends Component {
   render() {
-    return <article>
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </article>
+    const links = {
+      github: "https://github.com/ppg-husaria/pg_blog",
+      facebook: "https://www.facebook.com/PPGHusaria"
+    };
+
+    return <Jumbotron>
+      <article>
+        Check us out on <a href={links.github}>Github</a> and <a href={links.facebook}>Facebook</a>
+      </article>
+    </Jumbotron>
   }
 }
 
 class AppFooter extends Component {
   render() {
+    const createAuthorLink = (name, link, i) => <a href={link} key={i}>{name}</a>
+
+    const authors = intersperse([
+      ["Patryk Wertka", "https://www.facebook.com/spartanPAGE"],
+      ["Mateusz Janota", "https://www.facebook.com/lexonouri"]
+    ].map((author, i) => createAuthorLink(...author, i)), " ∧ ");
+
     return <footer>
-      Copyright © 2017 Patryk Wertka &and; Mateusz Janota
+      Copyright © 2017 {authors}
     </footer>
   }
 }
