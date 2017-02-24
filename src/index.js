b926b97a839182514f55f8eedfed380a6fd60439
spartanPAGE <spartanpageagain@gmail.com>
2017-02-24 15:50:35 +0100

    bootstrapped router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+
+import reducers from './reducers'
+
 import App from './App';
 import './index.css';
 
+// Add the reducer to your store on the `routing` key
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    routing: routerReducer
+  })
+)
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    { /* Tell the Router to use our enhanced history */ }
+    <Router history={history}>
+      <Route path="/" component={App}>
+         { /* <Route path="foo" component={Foo}/> */ }
+        { /* <Route path="bar" component={Bar}/> */ }
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('root')
-);
+)
